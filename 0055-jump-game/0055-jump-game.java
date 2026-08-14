ebad0d31ecbeb654eecc1b00078d4a8a6ada25ef
Sarvagya0602 <em2sarvagya@gmail.com>
2026-08-14 09:27:34 +0530

    Time: 2 ms (88.89%), Space: 47.7 MB (92.22%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,18 +1,14 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n=nums.length;
-        boolean[] visited=new boolean[nums.length];
+        int farthest=0;
         for(int i=0;i<n;i++){
-            if(i<=nums[0]) visited[i]=true;
-            else visited[i]=false;
+            if(i>farthest) return false;
+
+            farthest=Math.max(farthest,i+nums[i]);
+
+            if(farthest>=n-1) return true;
         }
-        for(int i=0;i<n;i++){
-            if(visited[i]) {
-                for(int j=0;j<=nums[i];j++){
-                    if(j+i<n) visited[j+i]=true;
-                }
-            }
-        }
-        return visited[n-1];
+        return false;
     }
 }
